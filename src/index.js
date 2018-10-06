401b4e674406f2a9709acff58fc8eece13702a3a
SBurnt <zip2004zip@gmail.com>
2018-10-06 13:25:07 +0300

    task advanced-zeros first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+
+  let newnum = base;
+  let arrMultipliers = [];
+
+  for (let i = 2; i < newnum; i * i) {
+    if (newnum % i === 0) {
+      arrMultipliers.push(i);
+      // console.log(arrMultipliers);
+      newnum = newnum / i;
+      // console.log(newnum);
+    }
+    else {
+      i++;
+      // console.log(i);
+    }
+  }
+
+  if (newnum != 1) {
+    // console.log(arrMultipliers);
+    arrMultipliers.push(newnum);
+    // console.log(arrMultipliers);
+  }
+
+  let maxMultiplier = Math.max.apply(null, arrMultipliers);
+  // console.log(maxMultiplier);
+  let degreeOfMultiplier = arrMultipliers.filter(item => item === maxMultiplier).length;
+  // console.log(degreeOfMultiplier);
+
+  var count = 0;
+  for (var i = maxMultiplier; number / i >= 1; i *= maxMultiplier) {
+    count += Math.floor(number / i);
+  }
+  zerosCount = Math.floor(count / degreeOfMultiplier);
+  // console.log(zerosCount);
+  return zerosCount;
 }
